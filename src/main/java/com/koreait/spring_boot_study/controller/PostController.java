170cdefd636507685e48ffca1066ed0854839391
nahyeon1220 <nahyeon03122@gmail.com>
2025-06-16 01:11:26 +0900

    Feat: RequestParam, RequestBody

--- src/main/java/com/koreait/spring_boot_study/controller/PostController.java
+++ src/main/java/com/koreait/spring_boot_study/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.koreait.spring_boot_study.controller;
 
 import com.koreait.spring_boot_study.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/post")
 public class PostController {
-    private final PostService postService;
+
+    //Autowired
+    @Autowired //필요한 객체를 자동으로 주입해주는 어노테이션
+    //오토와이드 보다는 생성자가 훨씬 더 안정성이 높음.
+    private PostService postService;
+    //PostService가 주입되기 전 시점에서 사용하게 되면, NPE이 발생할 수도 있음
+    //ex. 생성자에서 바로 사용, 서비스/레포지토리 어노테이션을 안 붙였을 때
+//    private final PostService postService;
 
     //Inversion Of Control -> 제어의 역전
     //객체 생성과 제어의 주도권을 개발자가 아닌, 스프링 부트가 갖는 것
@@ -33,9 +41,16 @@ public class PostController {
     //Dependency Injection -> DI
     //필요한 객체(의존성)를 직접 만들지 않고, 외부(스프링부트)에서 대신 넣어주는 것
 
-    public PostController(PostService postService) {
-        this.postService = postService;
-    }
+//    public PostController() {
+//        postService.getPost();
+//    }
+
+    //생성자 방식이 더 권장 -> 명시적이고 명확
+    //final이 있기 때문에 불변을 보장 -> 안정성
+    //생성자로 주입하게 되면 객체가 생성될 때, 필수로 의존성을 받아야 함
+    //그러면 이후 그 의존성을 바꿀 수 없어 안정적
+    //애초에 객체 생성이 되기도 전에 생성자를 통해 주입이 완료됨, 생성 전부터 준비가 완료
+
 
     @GetMapping("/get")
     public String getPost() {
